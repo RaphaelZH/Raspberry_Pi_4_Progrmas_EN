3d93fa61ff42c4cbc42a895808b1a7709d8221e1
haozhang <haozhang@me.com>
2021-04-03 22:58:50 +0200

    Update 1_LED.py

--- python_programs/1_LED.py
+++ python_programs/1_LED.py
@@ -5,22 +5,22 @@ LED_PIN = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
-# LED default setting
+# turn on this LED before making any input
 # GPIO.output(LED_PIN, GPIO.HIGH)
 
 # state = int(input(""))
 state = int(input("Enter 0 to power off the LED, 1 to power on the LED: "))
 
-# 0 --> power off
+# if the state is 0 --> power off the LED
 if state == 0:
     GPIO.output(LED_PIN, GPIO.LOW)
-# 1 --> power on
+# if the state is 1 --> power on the LED
 elif state == 1:
     GPIO.output(LED_PIN, GPIO.HIGH)
 else:
     print("Wrong state value : " + str(state))
+    # otherwise --> cleanup GPIOs and exit
     GPIO.cleanup()
-    # exit
     exit
 
 time.sleep(2)
