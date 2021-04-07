90a5efc0f50e55bf80d347fcac1d05c2858c0476
haozhang <haozhang@me.com>
2021-04-07 21:05:59 +0200

    Create 11_record_a_video_with_the_camera.py

---
+++ python_programs/11_record_a_video_with_the_camera.py
@@ -0,0 +1,19 @@
+from picamera import PiCamera
+import time
+
+camera = PiCamera()
+# setting resolution for the camera
+# this setting is requiring a tuple
+camera.resolution = (1280, 720)
+# setting rotation for the camera
+camera.rotation = 180
+time.sleep(2)
+
+# setting the directory to stock the videos recorded by the camera
+file_name = "/home/pi/Camera/video.h264"
+camera.start_recording(file_name)
+# different from the command in the Terminal,
+# the unit in here is not millisecond but second
+camera.wait_recording(10)
+camera.stop_recording()
+print("Done.")
