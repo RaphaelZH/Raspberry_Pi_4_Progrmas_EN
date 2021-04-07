6e07e140594453726269e865c2793a2194fe64be
haozhang <haozhang@me.com>
2021-04-07 23:09:15 +0200

    Create 12_take_a_series_of_photos_with_the_camera.py

---
+++ python_programs/12_take_a_series_of_photos_with_the_camera.py
@@ -0,0 +1,24 @@
+import os
+import time
+from picamera import PiCamera
+
+FOLDER_NAME = "/home/pi/Camera"
+
+# checking if the target directory exists; if not, create it
+if not os.path.exists(FOLDER_NAME):
+    os.mkdir(FOLDER_NAME)
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+camera.rotation = 180
+time.sleep(2)
+
+counter = 1
+
+while True:
+    # commanding Raspberry Pi to take pictures every 5 seconds
+    file_name = FOLDER_NAME + "/img" + str(counter) + ".jpg"
+    counter += 1
+    camera.capture(file_name)
+    print("New photo has been taken")
+    time.sleep(5)
